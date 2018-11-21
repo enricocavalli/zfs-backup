612ca6f8bebe0b904149a30ac42e4ef7a8617508
Enrico Cavalli <enrico.cavalli@gmail.com>
2018-11-21 16:24:08 +0100

    script for rsync only

---
+++ only-rsync.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+INSTALLDIR=$( (cd -P $(dirname $0) && pwd) )
+. "$INSTALLDIR/config-rsync"
+
+LIMIT_PRUNE=${LIMIT_PRUNE:-0}
+RPOOL=${MOUNT_POINT#/}
+
+
+
+SOURCES="/"
+if [ -f "$INSTALLDIR/sources.txt" ]; then
+  SOURCES="--files-from=\"$INSTALLDIR/sources.txt\" -r /"
+fi
+
+EXCLUSIONS="--exclude-from=\"$INSTALLDIR/default-exclusions.txt\""
+[ -f "$INSTALLDIR/exclusions.txt" ] && EXCLUSIONS="${EXCLUSIONS} --exclude-from=\"$INSTALLDIR/exclusions.txt\""
+
+mkdir -p "$INSTALLDIR/logs"
+
+global_return=0
+
+eval nice -n 20 rsync \
+-e \"'ssh -i $KEYFILEPATH'\" \
+--log-file=\"$INSTALLDIR/logs/sync.log\" \
+--fuzzy \
+--sparse \
+--delete \
+--delete-excluded \
+--recursive \
+--group \
+--owner \
+--links \
+--hard-links \
+--times \
+--devices \
+--specials \
+--verbose \
+--perms \
+--numeric-ids \
+--compress \
+--timeout=600 \
+--itemize-changes \
+--one-file-system \
+$EXTRA_RSYNC_OPTIONS \
+$EXCLUSIONS \
+$SOURCES $REMOTE_USER@$REMOTEHOST:$MOUNT_POINT/
+
+return=$?
+
+if [ $return -eq 0 -o $return -eq 24 ]; then
+
+  if [ ! -z "$PING_URL" ]; then
+    curl -fsS --retry 3 "${PING_URL}" >/dev/null
+  fi
+fi
+
+
+exit $return
